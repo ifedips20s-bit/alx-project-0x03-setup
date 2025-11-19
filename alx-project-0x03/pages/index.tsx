8892898a696e7a8201e7fd72df51b1296d2221a6
ifedips20s <ifedips20s@gmail.com>
2025-11-19 13:21:12 +0100

    Add Components files

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layouts/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="container mx-auto mt-32 text-center">
+        <h1 className="text-4xl font-bold">Welcome to Splash App</h1>
+      </div>
+    </Layout>
+  );
+}
